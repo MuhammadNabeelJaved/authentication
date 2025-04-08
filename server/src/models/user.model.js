017ec06d1b12a69c49f62dc24ab7b99d8e719852
Nabeel <muhammadnabeeljavede@gmail.com>
2025-04-08 23:23:13 +0500

    Implement password hashing, verification code generation, and refresh token methods in user model

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,4 +1,5 @@
 import { Schema, model } from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new Schema({
     username: {
@@ -44,6 +45,32 @@ const userSchema = new Schema({
         default: null,
     },
 }, { timestamps: true });
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateVerificationCode = function () {
+    const code = Math.floor(100000 + Math.random() * 900000).toString(); // 6 digit code
+    this.verificationCode = code;
+    this.verificationCodeExpiry = new Date(Date.now() + 10 * 60 * 1000); // 10 minutes 
+    return code;
+}
+
+userSchema.methods.isVerificationCodeValid = function (code) {
+    return this.verificationCode === code && this.verificationCodeExpiry > new Date();
+}
+
+userSchema.methods.generateRefreshToken = function () {
+   
+}
 
 
 const User = model("User", userSchema);
