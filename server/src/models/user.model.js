3445098fb4e194a4f3761159f1949fd040830c8b
Nabeel <muhammadnabeeljavede@gmail.com>
2025-04-08 21:50:57 +0500

    Add user model schema with fields for username, email, password, role, and verification

---
+++ server/src/models/user.model.js
@@ -0,0 +1,47 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'user'],
+        default: 'user',
+    },
+    refreshToken: {
+        type: String,
+        default: null,
+    },
+    profilePicture: {
+        type: String,
+        default: null,
+    },
+    bio: {
+        type: String,
+        default: null,
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+    verificationCode: {
+        type: String,
+        default: null,
+    },
+    verificationCodeExpiry: {
+        type: Date,
+        default: null,
+    },
+}, { timestamps: true });
