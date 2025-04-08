f8787c78804d537dfc9833e42317b08e937f4e5d
Nabeel <muhammadnabeeljavede@gmail.com>
2025-04-08 22:01:14 +0500

    Remove unique constraint from username field in user schema and add apiError utility class for standardized error handling

---
+++ server/src/utils/apiError.js
@@ -0,0 +1,18 @@
+class apiError extends Error {
+    constructor(message, statusCode, stack, errors) {
+        super(message);
+        this.statusCode = statusCode,
+            this.message = message || "Something went wrong";
+        this.stack = stack || null;
+        this.errors = errors || null;
+        this.data = null;
+        this.success = false;
+        this.name = this.constructor.name;
+        if (stack) {
+            this.stack = stack;
+        }
+        else {
+            this.stack = Error.captureStackTrace(this, this.constructor);
+        }
+    }
+}
