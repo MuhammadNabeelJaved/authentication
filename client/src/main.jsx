a677f1220b26491d3ac5d62d1aa43304ce1be88f
Nabeel <muhammadnabeeljavede@gmail.com>
2025-04-10 01:07:01 +0500

    Add react-router for routing and update main.jsx to use RouterProvider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import SignUpForm from "./components/Signup.jsx";
+import SignInForm from "./components/Signin.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: <App />,
+    children: [
+      { index: true, Component: SignIn },
+      { path: "login", Component: <SignInForm /> },
+      { path: "signup", Component: <SignUpForm /> },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
